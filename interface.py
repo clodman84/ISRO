b9b6e6a9943e7752fce548a9afe4b4ee0336beb4
clodman84 <77118531+clodman84@users.noreply.github.com>
2021-12-02 16:37:33 +0530

    added preview button

--- interface.py
+++ interface.py
@@ -1,12 +1,15 @@
-from tkinter import *
+from tkinter import Tk, Label, StringVar, Button, Entry, OptionMenu, Spinbox, Toplevel, IntVar, Canvas
 from tkcalendar import Calendar
-import datetime
+from datetime import datetime
 from functools import partial
+from PIL import ImageTk, Image
 import isro
+import asyncio
 
 
 class EntryWindow:
     def __init__(self):
+        """The window where people enter values, can be better, could be a frame, but I am not in the mood."""
         self.root = Tk()
         self.root.geometry("380x450")
         self.root.resizable(False, False)
@@ -22,7 +25,9 @@ class EntryWindow:
         for i, label in enumerate(labelNames):
             Label(self.root, text=label, font=self.font).place(x=20, y=20 + 40 * i)
 
-        TYPES = ["L1C_ASIA_MER_BIMG", "ISRO_BIMG", "ISRO_BRUH", "ISRO_NOOOOO"]
+        TYPES = ["L1C_ASIA_MER_BIMG", "L1C_SGP_3D_IR1", "L1C_SGP_DMP", "L1C_SGP_NMP", "L1C_ASIA_MER_RGB", "L1C_SGP_RGB",
+                 "L1B_STD_IR1", "L1B_STD_IR2", "L1B_STD_MIR", "L1B_STD_WV", "L1B_STD_VIS", "L1B_STD_SWIR",
+                 "L1B_STD_BT_IR1_TEMP"]
         option_value = StringVar(value=TYPES[0])
         self.fields = [Entry(self.root, font=self.font),
                        Button(self.root, text="Get Calendar Date", command=partial(self.get_date, 1, 60),
@@ -30,23 +35,22 @@ class EntryWindow:
                        Button(self.root, text="Get Calendar Date", command=partial(self.get_date, 2, 100),
                               font=self.font),
                        OptionMenu(self.root, option_value, *TYPES),
-                       Spinbox(self.root, from_=0, to=10000, textvariable=DoubleVar(value=24), font=self.font)]
+                       Spinbox(self.root, from_=0, to=10000, textvariable=IntVar(value=24), font=self.font)]
 
         for i, field in enumerate(self.fields):
             field.place(x=120, y=20 + 40 * i)
         self.fields[3] = option_value
-        now = datetime.datetime.now()
+        now = datetime.now()
         self.calendar = Calendar(self.root, selectmode='day', year=now.year, month=now.month, day=now.day,
                                  date_pattern='dd-mm-y')
         self.calendar.place(x=20, y=250)
-        Button(self.root, text='Preview', font=self.font).place(x=20, y=210)
+        Button(self.root, text='Preview', font=self.font, command=self.preview).place(x=20, y=210)
         Button(self.root, text='Run', font=self.font, command=self.make_vid).place(x=230, y=210)
 
     def get_date(self, index, y):
         var_ = self.calendar.get_date()
         self.settings[index] = var_
         Label(self.root, text=var_, font=self.font).place(x=270, y=y)
-        print(self.settings)
 
     def show_error(self, msg):
         newWindow = Toplevel(self.root)
@@ -54,18 +58,57 @@ class EntryWindow:
         Label(newWindow,
               text=msg).pack()
 
+    def preview(self):
+        if not self.get_settings():
+            self.show_error(msg='You have not entered all the values yet')
+            return
+
+        image_list = []
+        for i, date_string in enumerate(self.settings[1:3]):
+            d = datetime.strptime(date_string, "%d-%m-%Y")
+            date = d.strftime("%d%b").upper()
+            year = d.strftime("%Y")
+            a = asyncio.run(isro.preview(date, year, '1200', self.settings[3], i))
+            if a:
+                img = Image.open(f"{i}.jpg")
+                img = img.resize((500, 500))
+                img = ImageTk.PhotoImage(img)
+                image_list.append(img)
+
+        if not image_list:
+            self.show_error(msg="Could not retrieve any images for the given parameters")
+            return
+        index = 0
+        img = image_list[index]
+        preview_Window = Toplevel(self.root)
+        preview_Window.title(self.settings[3])
+        preview_Window.geometry('500x500')
+        preview_Window.resizable(False, False)
+        canvas = Canvas(preview_Window, width=500, height=500)
+        canvas.place(x=0, y=0)
+        canvas.create_image(0, 0, image=img, anchor='nw')
+
+        def changeImage():
+            nonlocal index, image_list, canvas
+            if index < len(image_list)-1:
+                index += 1
+            else:
+                index = 0
+            img = image_list[index]
+            canvas.create_image(0, 0, image=img, anchor='nw')
+
+        Button(preview_Window, text='Toggle', command=changeImage).place(x=430, y=450)
+
     def get_settings(self):
         name = self.fields[0].get()
-        print(name)
-        if name is None:
-            self.settings[0] = str(datetime.datetime.now())
+        if name == '':
+            self.settings[0] = datetime.now().strftime('%d-%m-%Y_%H-%M')
         else:
             self.settings[0] = name
         self.settings[3] = self.fields[3].get()
         frame_rate = self.fields[4].get()
         if frame_rate == 0:
             self.show_error(msg='Frame-rate cannot be zero')
-            pass
         else:
             self.settings[4] = int(frame_rate)
         return all(self.settings)
@@ -74,6 +117,7 @@ class EntryWindow:
         if self.get_settings():
             video = isro.TimeLapse(*self.settings)
             video.video()
+            del video
         else:
             self.show_error(msg='You have not entered all the values yet')
 
--- isro.py
+++ isro.py
@@ -7,6 +7,8 @@ import cv2
 
 class TimeLapse:
     def __init__(self, name, start_date, end_date, type_, frame_rate=24):
+        """Works by creating a list of urls with the given parameters of date and type amd then fetches them to
+        stitch together into a video."""
         self.name = name
         self.type_ = type_
         self.frame_rate = frame_rate
@@ -88,7 +90,6 @@ class TimeLapse:
             image_list.append(img)
         height, width = image_list[0].shape[0:2]
         size = (width, height)
-        print(size)
         out = cv2.VideoWriter(
             f"Videos/{self.name}.avi",
             cv2.VideoWriter_fourcc(*"DIVX"),
@@ -97,8 +98,54 @@ class TimeLapse:
         )
 
         print("Creating Video")
-        for i in range(len(image_list)):
-            out.write(image_list[i])
+        for image in image_list:
+            out.write(image)
         out.release()
+        del out
+        del image_files
         print("DONE!")
 
+
+async def preview(date, year, time, type_, file_name):
+    """To get the preview images when the preview button is clicked.
+    Copy pasted from https://github.com/clodman84/AternosReborn/blob/master/space.py"""
+    count = 0
+    urls = []
+    mosdacString = 'https://mosdac.gov.in/look/3D_IMG/gallery'
+    if int(time[2:]) >= 30:
+        time = str(int(time[0:2]) + 1) + "00"
+        if len(time) == 3:
+            time = "0" + time
+    else:
+        time = time[0:2] + "30"
+    while int(time) > 0 and count <= 10:
+        count += 1
+        if time[2:] == "00":
+            time = str(int(time[:-2]) - 1) + "30"
+        else:
+            time = time[:-2] + "00"
+        if len(time) == 3:
+            time = "0" + time
+        url = f"{mosdacString}/{year}/{date}/3DIMG_{date}{year}_{time}_{type_}.jpg"
+        urls.append(url)
+        # trying 29 and 59
+        if time[2:] == "00":
+            NEWtime = str(int(time[:-2]) - 1) + "59"
+        else:
+            NEWtime = time[:-2] + "29"
+        if len(NEWtime) == 3:
+            NEWtime = "0" + NEWtime
+            url = f"{mosdacString}/{year}/{date}/3DIMG_{date}{year}_{NEWtime}_{type_}.jpg"
+            urls.append(url)
+
+    async with httpx.AsyncClient(timeout=None) as client:
+        tasks = (client.get(url) for url in urls)
+        reqs = await asyncio.gather(*tasks)
+
+    for i in reqs:
+        if i.status_code == 200:
+            with open(f"{file_name}.jpg", "wb") as file:
+                file.write(i.content)
+            return True
+
+    return False
