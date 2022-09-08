7850b4e968b85af37d97409396f7604852fa44e4
clodman84 <77118531+clodman84@users.noreply.github.com>
2022-09-09 00:35:42 +0530

    New GUI and better URL logic

--- isro.py
+++ isro.py
@@ -1,31 +1,27 @@
 import httpx
 import asyncio
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 from subprocess import run
-import Database
+import logging
+
+logger = logging.getLogger("ISRO.Generator")
 
 
 class TimeLapse:
-    def __init__(self, name, start_date, end_date, type_, frame_rate=24):
-        """Works by creating a list of urls with the given parameters of date and type amd then fetches them to
-        stitch together into a video."""
-        self.name = name
-        self.type_ = type_
+    def __init__(self, name: str, start_date: datetime, end_date: datetime, prod: str, frame_rate=24):
+        self.name = name  # name of the video
+        self.prod = prod  # "L1C_ASIA_MER_BIMG","L1C_ASIA_MER_BIMG_KARNATAKA", etc
         self.frame_rate = frame_rate
-        start = datetime.strptime(start_date, "%d-%m-%Y")
-        end = datetime.strptime(end_date, "%d-%m-%Y")
-        self.dateList = [
-            start + timedelta(days=x) for x in range(0, (end - start).days)
-        ]
-        self.urls = self.generateURLS()
+        self.start = start_date.replace(hour=0)
+        self.end = end_date.replace(hour=23)  # time goes forward and the past was the beginning
+        self.urls = self.getURLS()
         self.createDir()
-        self.fileIndex = 0  # made this a class variable for the database thing
+        self.fileIndex = 0
 
-    # creating the folders
     def createDir(self):
         if os.path.isdir(f"Images/{self.name}"):
-            print(f"Images/{self.name} already exists, the file will be overwritten")
+            logger.warning(f"Images/{self.name} already exists, the file will be overwritten")
         else:
             os.makedirs(f"Images/{self.name}")
 
@@ -33,79 +29,71 @@ class TimeLapse:
             pass
         else:
             os.mkdir("Videos")
-        print("Folders Created!")
+        logger.info("Folders Created!")
 
-    def generateURLS(self):
-        # this can be better I think
-        minutes = ("00", "59", "30", "29")   # the satellite takes images in a 30 minute time-offset
-        urls = []
-        mosdacString = "https://mosdac.gov.in/look/3D_IMG/preview"
-        print("Creating URLs")
-        for d in self.dateList:  # looping through each day in the date-list created to generate urls
+    def getURLS(self):
+        logger.info("Getting URLs!")
+        mosdacString = "https://mosdac.gov.in/look/"
+        smh = '\n'
+        start_time = self.start.strftime("%d%b%Y").upper()
+        end_time = self.end.strftime("%Y-%m-%d")
+        count = (self.end - self.start).seconds / 1800
+        count = 48 if count == 0 else count  # potential for having minute-level granularity in the future
+        json = {"prod": f"3DIMG_*_{self.prod}_V*.jpg", "st_date": end_time, "count": count}
+        logger.debug(f"URL Parameters - \nStart: {start_time}\nEnd: {end_time}\nCount: {count}\nProd: {json['prod']}")
 
-            date = d.strftime("%d%b").upper()
-            year = d.strftime("%Y")
-            for hour in range(0, 23):
-                # increments the time by 30 minutes
-                if hour < 10:
-                    hour = "0" + str(hour)
-                else:
-                    hour = str(hour)
-                for minute in minutes:
-                    time = hour + minute
-                    url = f"{mosdacString}/{year}/{date}/3DIMG_{date}{year}_{time}_{self.type_}.jpg"
-                    urls.append(url)
+        data = httpx.post("https://www.mosdac.gov.in/gallery/getImage.php", json=json).json()[0]
+        logger.debug(f"Length of URL Data Received: {len(data)}")
+        index = data.find(start_time)
+        logger.debug(f"Index of start date: {index}")
+        index = 0 if index == -1 else index
+        data = data[index:]
+        urls = [mosdacString + url for url in data.split(',')]
+        logger.debug(f"Length of URL-list = {len(urls)},\nSample: {smh.join(urls[:2])}")
+        logger.info("Received and Parsed URLs!")
         return urls
 
     async def getImages(self):
-        # Breaking the downloads into chunks for stability and less ram usage
+        # Breaking the downloads into chunks
         n = 850
         chunks = [self.urls[i * n: (i + 1) * n] for i in range((len(self.urls) + n - 1) // n)]
         for i, chunk in enumerate(chunks):
             while True:
                 try:
-                    print(f"Downloading Chunk {i+1}/{len(chunks)}...")
+                    logger.info(f"Downloading Chunk {i + 1}/{len(chunks)}...")
                     async with httpx.AsyncClient(timeout=None) as client:
                         sub_tasks = (client.get(url) for url in chunk)
                         requests = await asyncio.gather(*sub_tasks)
 
-                    requests[:] = [i for i in requests if i.status_code == 200]     # filtering out all failed requests
+                    requests = [i for i in requests if i.status_code == 200]  # filtering out all failed requests
 
                     for image in requests:
-                        ImageName = f"{self.type_}_{self.fileIndex}.jpg"
+                        ImageName = f"{self.prod}_{self.fileIndex}.jpg"
                         with open(f"Images/{self.name}/{ImageName}", "wb") as file:
                             file.write(image.content)
                         self.fileIndex += 1
 
-                except httpx.ConnectError:      # random error \_(o_o)_/
-                    print("ConnectError encountered, trying again...")
+                except httpx.ConnectError:  # random error \_(o_o)_/
+                    logger.error("ConnectError encountered, trying again...")
                     continue
                 break
 
     def video(self):
         asyncio.run(self.getImages())
-        # using ffmpeg instead of OpenCV because OpenCV videos are too big,
-        # and they take up far too much ram and its too slow
-        print('Generating Video...')
+        logger.info('Generating Video...')
         args = ['ffmpeg', '-loglevel', 'quiet', '-stats', '-framerate', str(self.frame_rate), '-i',
-                f'./Images/{self.name}/{self.type_}_%d.jpg', '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2', '-vcodec',
+                f'./Images/{self.name}/{self.prod}_%d.jpg', '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2', '-vcodec',
                 'libx264', '-y', '-an', f'./Videos/{self.name}.mp4']
         run(args=args)
-        print('Adding Run to Database...')
-        start = self.dateList[0].strftime("%Y-%m-%d")
-        end = self.dateList[-1].strftime("%Y-%m-%d")
-        img_no = len(self.dateList) * 48
-        record = self.fileIndex
-        db = Database.Database()
-        db.recordData(self.type_, img_no, record, start, end)
-        print("DONE!")
+        logger.info('Done!')
 
 
+# TODO: Get rid of this.
 async def preview(date, year, type_, file_name):
     """To get the preview images when the preview button is clicked.
     Copy pasted from https://github.com/clodman84/AternosReborn/blob/master/space.py"""
 
-    minutes = ("00", "59", "30", "29")  # the satellite takes images in a 30 minute time-offset
+    minutes = ("00", "59", "30", "29")
     urls = []
     mosdacString = "https://mosdac.gov.in/look/3D_IMG/preview"
 
