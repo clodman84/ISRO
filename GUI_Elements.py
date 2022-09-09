1d9f3b1ed6e4011eb1d026a7ac06c5d9fc9dac90
clodman84 <77118531+clodman84@users.noreply.github.com>
2022-09-09 15:12:04 +0530

    update

--- GUI_Elements.py
+++ GUI_Elements.py
@@ -16,12 +16,19 @@ def modalMessage(message):
     if message in ModalHiddenList:
         return
     with dpg.mutex():
-        with dpg.window(modal=True, autosize=True, no_resize=True, no_title_bar=True) as warning:
+        with dpg.window(
+            modal=True, autosize=True, no_resize=True, no_title_bar=True
+        ) as warning:
             dpg.add_text(f"{message}", wrap=200)
             dpg.add_separator()
-            dpg.add_checkbox(label="Don't show this again.", callback=lambda: ModalHiddenList.append(message))
+            dpg.add_checkbox(
+                label="Don't show this again.",
+                callback=lambda: ModalHiddenList.append(message),
+            )
             with dpg.group(horizontal=True):
-                dpg.add_button(label="Okay", width=75, callback=lambda: dpg.delete_item(warning))
+                dpg.add_button(
+                    label="Okay", width=75, callback=lambda: dpg.delete_item(warning)
+                )
     dpg.split_frame()
     modal_dimensions = dpg.get_item_rect_size(warning)
     window_dimensions = dpg.get_item_rect_size("Primary Window")
@@ -35,22 +42,45 @@ class DataEntry:
         self.thread_running = False
         dpg.add_input_text(label="Video Name", width=300, tag="name")
 
-        types = ("L1C_ASIA_MER_BIMG", "L1C_ASIA_MER_BIMG_KARNATAKA", "L1C_SGP_3D_IR1", "L1C_SGP_DMP", "L1C_SGP_NMP",
-                 "L1C_ASIA_MER_RGB", "L1C_SGP_RGB", "L1B_STD_IR1", "L1B_STD_IR2", "L1B_STD_MIR", "L1B_STD_WV",
-                 "L1B_STD_VIS", "L1B_STD_SWIR", "L1B_STD_BT_IR1_TEMP")
+        types = (
+            "L1C_ASIA_MER_BIMG",
+            "L1C_ASIA_MER_BIMG_KARNATAKA",
+            "L1C_SGP_3D_IR1",
+            "L1C_SGP_DMP",
+            "L1C_SGP_NMP",
+            "L1C_ASIA_MER_RGB",
+            "L1C_SGP_RGB",
+            "L1B_STD_IR1",
+            "L1B_STD_IR2",
+            "L1B_STD_MIR",
+            "L1B_STD_WV",
+            "L1B_STD_VIS",
+            "L1B_STD_SWIR",
+            "L1B_STD_BT_IR1_TEMP",
+        )
         dpg.add_combo(types, label="Product", tag="product", width=300)
         with dpg.tooltip(dpg.last_item()):
             dpg.add_text("This is the type of image you want to generate.")
 
-        dpg.add_input_int(label="Framerate", tag="framerate", width=300, default_value=24)
-        dpg.add_input_int(label="Chunk Size", tag="chunk_size", width=300, default_value=850)
+        dpg.add_input_int(
+            label="Framerate", tag="framerate", width=300, default_value=24
+        )
+        dpg.add_input_int(
+            label="Chunk Size", tag="chunk_size", width=300, default_value=850
+        )
         with dpg.tooltip(dpg.last_item()):
-            dpg.add_text("The number of images that will be downloaded in parallel. "
-                         "Higher is faster, lower is more stable")
+            dpg.add_text(
+                "The number of images that will be downloaded in parallel. "
+                "Higher is faster, lower is more stable"
+            )
         dpg.add_separator()
 
         now = datetime.now()
-        default_date = {"year": now.year % 100 + 100, "month": now.month - 1, "month_day": now.day}
+        default_date = {
+            "year": now.year % 100 + 100,
+            "month": now.month - 1,
+            "month_day": now.day,
+        }
         with dpg.collapsing_header(label="Start Date", default_open=True):
             dpg.add_date_picker(tag="start", default_value=default_date)
         with dpg.collapsing_header(label="End Date", default_open=True):
@@ -66,13 +96,24 @@ class DataEntry:
             with dpg.tooltip(dpg.last_item()):
                 dpg.add_text("Click to view the first and last image of the timelapse.")
 
-            dpg.add_button(label="Terminate", callback=lambda: None, width=80, show=False, tag="terminate")
+            dpg.add_button(
+                label="Terminate",
+                callback=lambda: None,
+                width=80,
+                show=False,
+                tag="terminate",
+            )
             with dpg.tooltip(dpg.last_item()):
                 dpg.add_text("Click to terminate image download.")
 
     def validate(self):
-        valid = [False, False, False, False]  # [name, dates, prod, chunk_size], True if the values are valid
-        if self.name != '':
+        valid = [
+            False,
+            False,
+            False,
+            False,
+        ]  # [name, dates, prod, chunk_size], True if the values are valid
+        if self.name != "":
             valid[0] = True
         else:
             logger.error("Name cannot be empty.")
@@ -85,7 +126,7 @@ class DataEntry:
         else:
             valid[1] = True
 
-        if self.prod != '':
+        if self.prod != "":
             valid[2] = True
         else:
             logger.error("Please select a product.")
@@ -99,7 +140,9 @@ class DataEntry:
             logger.info("All inputs are valid!")
             return True
         else:
-            modalMessage("Something is wrong with the values you entered.\n\nCheck the logs on the right.")
+            modalMessage(
+                "Something is wrong with the values you entered.\n\nCheck the logs on the right."
+            )
             return False
 
     def get_settings(self):
@@ -113,7 +156,9 @@ class DataEntry:
         if not self.validate():
             return
         video = isro.TimeLapse(*self.get_settings())
-        vid_thread = threading.Thread(target=video.video, args=(lambda: self.thread_running,))
+        vid_thread = threading.Thread(
+            target=video.video, args=(lambda: self.thread_running,)
+        )
         self.thread_running = True
         vid_thread.daemon = True
         vid_thread.start()
@@ -138,7 +183,9 @@ class DataEntry:
 
     @property
     def name(self):
-        return '_'.join(dpg.get_value("name").split())  # don't want to have any spaces in the name
+        return "_".join(
+            dpg.get_value("name").split()
+        )  # don't want to have any spaces in the name
 
     @property
     def dates(self):
@@ -167,46 +214,74 @@ class DataEntry:
 class ImageWindow:
     def __init__(self, directory):
         self.image_list = sorted(glob(f"{directory}*.jpg"))
-        self.image_list.sort(key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])
+        self.image_list.sort(
+            key=lambda x: [int(c) if c.isdigit() else c for c in re.split(r"(\d+)", x)]
+        )
         if len(self.image_list) == 0:
             logger.error(f"No .jpg found in {directory}")
-            modalMessage(f"No images ending in .jpg were found in {directory}, perhaps you moved them.")
+            modalMessage(
+                f"No images ending in .jpg were found in {directory}, perhaps you moved them."
+            )
             return
         self.index = 0
 
-        with dpg.window(label='ImageWindow', pos=(20, 100), width=500, height=500, no_title_bar=True) as self.window_id:
+        with dpg.window(
+            label="ImageWindow", pos=(20, 100), width=500, height=500, no_title_bar=True
+        ) as self.window_id:
             with dpg.group(horizontal=True, parent=self.window_id):
                 dpg.add_button(label="Previous", callback=lambda: self._loadTexture(-1))
                 dpg.add_button(label="Next", callback=lambda: self._loadTexture(1))
-                dpg.add_button(label="Play", tag=f'play-{self.window_id}')
-                dpg.add_button(label="Rewind", tag=f'rewind-{self.window_id}')
+                dpg.add_button(label="Play", tag=f"play-{self.window_id}")
+                dpg.add_button(label="Rewind", tag=f"rewind-{self.window_id}")
                 dpg.add_button(label="Close", callback=lambda: self._close())
 
-        with dpg.child_window(parent=self.window_id, autosize_x=True, autosize_y=True) as self.image_window:
+        with dpg.child_window(
+            parent=self.window_id, autosize_x=True, autosize_y=True
+        ) as self.image_window:
             width, height, channels, data = dpg.load_image(self.image_list[self.index])
             with dpg.texture_registry() as self.registry:
                 dpg.add_raw_texture(width, height, data, tag=f"image-{self.window_id}")
-            with dpg.plot(label=self.image_list[self.index], parent=self.image_window, width=-1, height=-1,
-                          equal_aspects=True) as self.image:
+            with dpg.plot(
+                label=self.image_list[self.index],
+                parent=self.image_window,
+                width=-1,
+                height=-1,
+                equal_aspects=True,
+            ) as self.image:
                 # Dummy bar series to trick the scale and zoom out to fit the image
-                dpg.add_plot_axis(dpg.mvYAxis, tag=f'y-{self.window_id}')
-                dpg.add_bar_series([-width / 2, 0, width / 2], [0, 0, 0], weight=1, parent=f"y-{self.window_id}")
-                dpg.draw_image(f"image-{self.window_id}", (-width / 2, height / 2), (width / 2, -height / 2))
+                dpg.add_plot_axis(dpg.mvYAxis, tag=f"y-{self.window_id}")
+                dpg.add_bar_series(
+                    [-width / 2, 0, width / 2],
+                    [0, 0, 0],
+                    weight=1,
+                    parent=f"y-{self.window_id}",
+                )
+                dpg.draw_image(
+                    f"image-{self.window_id}",
+                    (-width / 2, height / 2),
+                    (width / 2, -height / 2),
+                )
 
         with dpg.item_handler_registry(tag=f"playHandler-{self.window_id}"):
             dpg.add_item_active_handler(callback=lambda: self._loadTexture(1))
         with dpg.item_handler_registry(tag=f"rewindHandler-{self.window_id}"):
             dpg.add_item_active_handler(callback=lambda: self._loadTexture(-1))
 
-        dpg.bind_item_handler_registry(f'play-{self.window_id}', f'playHandler-{self.window_id}')
-        dpg.bind_item_handler_registry(f'rewind-{self.window_id}', f'rewindHandler-{self.window_id}')
+        dpg.bind_item_handler_registry(
+            f"play-{self.window_id}", f"playHandler-{self.window_id}"
+        )
+        dpg.bind_item_handler_registry(
+            f"rewind-{self.window_id}", f"rewindHandler-{self.window_id}"
+        )
 
     def _loadTexture(self, i):
-        if (self.index > 0 and i == -1) or (self.index < len(self.image_list) - 1 and i == 1):
+        if (self.index > 0 and i == -1) or (
+            self.index < len(self.image_list) - 1 and i == 1
+        ):
             self.index += i
         filename = self.image_list[self.index]
         _, _, _, data = dpg.load_image(filename)
-        dpg.set_value(f'image-{self.window_id}', data)
+        dpg.set_value(f"image-{self.window_id}", data)
         dpg.configure_item(self.image, label=filename)
 
     def _close(self):
@@ -219,20 +294,34 @@ class Explorer:
     def __init__(self, parent):
         self.window_id = parent
         self.table = None
-        dpg.add_button(label='Refresh', callback=self._loadDirectories, parent=self.window_id)
+        dpg.add_button(
+            label="Refresh", callback=self._loadDirectories, parent=self.window_id
+        )
         imageFolders = []
-        if os.path.isdir('.\\Images'):
-            imageFolders = os.listdir('.\\Images')
+        if os.path.isdir(".\\Images"):
+            imageFolders = os.listdir(".\\Images")
         if not imageFolders:
             logger.warning("No image folders were found.")
-            with dpg.child_window(parent=self.window_id, autosize_x=True, autosize_y=True, tag='ExpWindow'):
+            with dpg.child_window(
+                parent=self.window_id, autosize_x=True, autosize_y=True, tag="ExpWindow"
+            ):
                 dpg.add_text("Make some videos and they will appear here.", wrap=0)
             return
 
-        with dpg.child_window(parent=self.window_id, autosize_x=True, autosize_y=True, tag='ExpWindow') as window:
-            with dpg.table(label=None, parent=window, header_row=False, row_background=True,
-                           borders_innerH=True, borders_outerH=True, borders_innerV=True, borders_outerV=True,
-                           delay_search=True) as self.table:
+        with dpg.child_window(
+            parent=self.window_id, autosize_x=True, autosize_y=True, tag="ExpWindow"
+        ) as window:
+            with dpg.table(
+                label=None,
+                parent=window,
+                header_row=False,
+                row_background=True,
+                borders_innerH=True,
+                borders_outerH=True,
+                borders_innerV=True,
+                borders_outerV=True,
+                delay_search=True,
+            ) as self.table:
                 dpg.add_table_column(width_fixed=True)
                 dpg.add_table_column(width_fixed=False)
                 dpg.add_table_column(width_fixed=True)
@@ -240,23 +329,37 @@ class Explorer:
                     with dpg.table_row():
                         dpg.add_text(str(index + 1))
                         dpg.add_text(directory)
-                        dpg.add_button(label='View', user_data=directory,
-                                       callback=lambda s, a, u: ImageWindow(f".\\Images\\{u}\\"))
+                        dpg.add_button(
+                            label="View",
+                            user_data=directory,
+                            callback=lambda s, a, u: ImageWindow(f".\\Images\\{u}\\"),
+                        )
 
     def _loadDirectories(self):
         imageFolders = []
-        if os.path.isdir('.\\Images'):
-            imageFolders = os.listdir('.\\Images')
+        if os.path.isdir(".\\Images"):
+            imageFolders = os.listdir(".\\Images")
         if not imageFolders:
-            dpg.delete_item('ExpWindow')
+            dpg.delete_item("ExpWindow")
             logger.warning("No image folders were found.")
-            with dpg.child_window(parent=self.window_id, autosize_x=True, autosize_y=True, tag='ExpWindow'):
+            with dpg.child_window(
+                parent=self.window_id, autosize_x=True, autosize_y=True, tag="ExpWindow"
+            ):
                 dpg.add_text("Make some videos and they will appear here.", wrap=0)
             return
         if self.table:
             dpg.delete_item(self.table)
-        with dpg.table(label=None, parent="ExpWindow", header_row=False, row_background=True, borders_innerH=True,
-                       borders_outerH=True, borders_innerV=True, borders_outerV=True, delay_search=True) as self.table:
+        with dpg.table(
+            label=None,
+            parent="ExpWindow",
+            header_row=False,
+            row_background=True,
+            borders_innerH=True,
+            borders_outerH=True,
+            borders_innerV=True,
+            borders_outerV=True,
+            delay_search=True,
+        ) as self.table:
             dpg.add_table_column(width_fixed=True)
             dpg.add_table_column(width_fixed=False)
             dpg.add_table_column(width_fixed=True)
@@ -264,8 +367,11 @@ class Explorer:
                 with dpg.table_row():
                     dpg.add_text(str(index + 1))
                     dpg.add_text(directory)
-                    dpg.add_button(label='View', user_data=directory,
-                                   callback=lambda s, a, u: ImageWindow(f".\\Images\\{u}\\"))
+                    dpg.add_button(
+                        label="View",
+                        user_data=directory,
+                        callback=lambda s, a, u: ImageWindow(f".\\Images\\{u}\\"),
+                    )
 
 
 class Logger(logging.Handler):
@@ -279,14 +385,26 @@ class Logger(logging.Handler):
         self.window_id = parent
 
         with dpg.group(horizontal=True, parent=self.window_id):
-            dpg.add_checkbox(label="Auto-scroll", default_value=True,
-                             callback=lambda sender: self.auto_scroll(dpg.get_value(sender)))
-            dpg.add_button(label="Clear", callback=lambda: dpg.delete_item(self.filter_id, children_only=True))
+            dpg.add_checkbox(
+                label="Auto-scroll",
+                default_value=True,
+                callback=lambda sender: self.auto_scroll(dpg.get_value(sender)),
+            )
+            dpg.add_button(
+                label="Clear",
+                callback=lambda: dpg.delete_item(self.filter_id, children_only=True),
+            )
 
-        dpg.add_input_text(label="Filter (inc, -exc)",
-                           callback=lambda sender: dpg.set_value(self.filter_id, dpg.get_value(sender)),
-                           parent=self.window_id)
-        self.child_id = dpg.add_child_window(parent=self.window_id, autosize_x=True, autosize_y=True)
+        dpg.add_input_text(
+            label="Filter (inc, -exc)",
+            callback=lambda sender: dpg.set_value(
+                self.filter_id, dpg.get_value(sender)
+            ),
+            parent=self.window_id,
+        )
+        self.child_id = dpg.add_child_window(
+            parent=self.window_id, autosize_x=True, autosize_y=True
+        )
         self.filter_id = dpg.add_filter_set(parent=self.child_id)
 
         with dpg.theme() as self.debug_theme:
@@ -334,7 +452,9 @@ class Logger(logging.Handler):
         elif level == 50:
             theme = self.critical_theme
 
-        new_log = dpg.add_text(message, parent=self.filter_id, filter_key=message, wrap=0)
+        new_log = dpg.add_text(
+            message, parent=self.filter_id, filter_key=message, wrap=0
+        )
         dpg.bind_item_theme(new_log, theme)
         if self._auto_scroll:
             dpg.set_y_scroll(self.child_id, -1.0)
--- isro.py
+++ isro.py
@@ -10,20 +10,32 @@ logger = logging.getLogger("ISRO.Generator")
 
 
 class TimeLapse:
-    def __init__(self, name: str, start_date: datetime, end_date: datetime, prod: str, frame_rate=24, chunk_size=850):
+    def __init__(
+        self,
+        name: str,
+        start_date: datetime,
+        end_date: datetime,
+        prod: str,
+        frame_rate=24,
+        chunk_size=850,
+    ):
         self.name = name  # name of the video
         self.prod = prod  # "L1C_ASIA_MER_BIMG","L1C_ASIA_MER_BIMG_KARNATAKA", etc
         self.frame_rate = frame_rate
         self.chunk_size = chunk_size
         self.start = start_date.replace(hour=0)
-        self.end = end_date.replace(hour=23)  # time goes forward and the past was the beginning
+        self.end = end_date.replace(
+            hour=23
+        )  # time goes forward and the past was the beginning
         self.urls = self.getURLS()
         self.createDir()
         self.fileIndex = 0
 
     def createDir(self):
         if os.path.isdir(f"Images/{self.name}"):
-            logger.warning(f"Images/{self.name} already exists, the files will be overwritten")
+            logger.warning(
+                f"Images/{self.name} already exists, the files will be overwritten"
+            )
         else:
             os.makedirs(f"Images/{self.name}")
 
@@ -36,16 +48,26 @@ class TimeLapse:
     def getURLS(self):
         logger.info("Getting URLs!")
         mosdacString = "https://mosdac.gov.in/look/"
-        smh = '\n'
+        smh = "\n"
         start_time = self.start.strftime("%d%b%Y").upper()
         end_time = self.end.strftime("%Y-%m-%d")
         count = (self.end - self.start).total_seconds() / 1800
-        count = 48 if count == 0 else count  # potential for having minute-level granularity in the future
-        json = {"prod": f"3DIMG_*_{self.prod}_V*.jpg", "st_date": end_time, "count": count}
-        logger.debug(f"URL Parameters - \nStart: {start_time}\nEnd: {end_time}\nCount: {count}\nProd: {json['prod']}")
+        count = (
+            48 if count == 0 else count
+        )  # potential for having minute-level granularity in the future
+        json = {
+            "prod": f"3DIMG_*_{self.prod}_V*.jpg",
+            "st_date": end_time,
+            "count": count,
+        }
+        logger.debug(
+            f"URL Parameters - \nStart: {start_time}\nEnd: {end_time}\nCount: {count}\nProd: {json['prod']}"
+        )
 
         try:
-            data = httpx.post("https://www.mosdac.gov.in/gallery/getImage.php", json=json).json()[0]
+            data = httpx.post(
+                "https://www.mosdac.gov.in/gallery/getImage.php", json=json
+            ).json()[0]
         except httpx.ConnectError as e:
             logger.error(f"{e}. Check your internet connection.")
             return
@@ -55,15 +77,17 @@ class TimeLapse:
         logger.debug(f"Index of start date: {index}")
         index = 0 if index == -1 else index
         data = data[index:]
-        urls = [mosdacString + url for url in data.split(',')]
-        logger.debug(f"Length of URL-list = {len(urls)},\nSample: {smh.join(urls[:2])}")
+        urls = [mosdacString + url for url in data.split(",")]
+        logger.debug(f"Length of URL-list = {len(urls)},\nSample:\n{smh.join(urls[:2])}")
         logger.info("Received and Parsed URLs!")
         return urls
 
     async def getImages(self, continue_running: Callable):
         # Breaking the downloads into chunks
         n = self.chunk_size
-        chunks = [self.urls[i * n: (i + 1) * n] for i in range((len(self.urls) + n - 1) // n)]
+        chunks = [
+            self.urls[i * n : (i + 1) * n] for i in range((len(self.urls) + n - 1) // n)
+        ]
         for i, chunk in enumerate(chunks):
             while True:
                 if not continue_running():
@@ -75,21 +99,30 @@ class TimeLapse:
                     async with httpx.AsyncClient(timeout=None) as client:
                         sub_tasks = (client.get(url) for url in chunk)
                         requests = await asyncio.gather(*sub_tasks)
-
-                    requests = [i for i in requests if i.status_code == 200]  # filtering out all failed requests
-
+                    logger.debug(f"Received chunk {i + 1}")
+                    requests = [
+                        i for i in requests if i.status_code == 200
+                    ]  # filtering out all failed requests
+                    logger.debug("Failed requests filtered.")
                     for image in requests:
                         ImageName = f"{self.prod}_{self.fileIndex}.jpg"
+                        logger.debug(f"Writing file {ImageName}")
                         with open(f"Images/{self.name}/{ImageName}", "wb") as file:
                             file.write(image.content)
                         self.fileIndex += 1
 
                 except httpx.ConnectError as e:  # random error \_(o_o)_/
-                    logger.error(f"{e}, (If the problem persists try reducing your chunk size). Trying again...")
+                    logger.error(
+                        f"{e}, (If the problem persists try reducing your chunk size). Trying again..."
+                    )
                     continue
                 except RuntimeError as e:
-                    logger.error(f"{e}, (If the problem persists try reducing your chunk size). Trying again...")
+                    logger.error(
+                        f"{e}, (If the problem persists try reducing your chunk size). Trying again..."
+                    )
                     continue
+                except Exception as e:
+                    logger.error(f"{e}")
                 break
 
     def video(self, continue_running: Callable):
@@ -97,9 +130,23 @@ class TimeLapse:
         if not continue_running():
             logger.warning("Stopped Video Generation...")
             return
-        logger.info('Generating Video...')
-        args = ['ffmpeg', '-loglevel', 'quiet', '-stats', '-framerate', str(self.frame_rate), '-i',
-                f'./Images/{self.name}/{self.prod}_%d.jpg', '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2', '-vcodec',
-                'libx264', '-y', '-an', f'./Videos/{self.name}.mp4']
+        logger.info("Generating Video...")
+        args = [
+            "ffmpeg",
+            "-loglevel",
+            "quiet",
+            "-stats",
+            "-framerate",
+            str(self.frame_rate),
+            "-i",
+            f"./Images/{self.name}/{self.prod}_%d.jpg",
+            "-vf",
+            "pad=ceil(iw/2)*2:ceil(ih/2)*2",
+            "-vcodec",
+            "libx264",
+            "-y",
+            "-an",
+            f"./Videos/{self.name}.mp4",
+        ]
         run(args=args)
-        logger.info('Done!')
+        logger.info("Done!")
