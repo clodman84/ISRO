058a6890034c381304abd70f90322319ca8eef29
clodman84 <cshourin@gmail.com>
2023-08-03 11:18:41 +0530

    product -> node in treeselector prompts

--- GUI/treeselector.py
+++ GUI/treeselector.py
@@ -22,7 +22,7 @@ class TreeSelector:
         self.selected_node = None
         a = time.perf_counter()
         self.status_text = dpg.add_text(
-            "Select a product type from the dropdown menu...", parent=parent
+            "Select a node from the dropdown menu...", parent=parent
         )
         dpg.add_separator()
         self._render(root, parent)
@@ -31,7 +31,7 @@ class TreeSelector:
 
     def click_callback(self, sender, app_data, user_data: anytree.Node):
         self.selected_node = user_data
-        dpg.set_value(self.status_text, f"Selected Product: {user_data.name}")
+        dpg.set_value(self.status_text, f"Selected node: {user_data.name}")
 
     def get_node(self):
         if not self.selected_node:
